1c765b18a88356d02d504f058e813dc9eb7964c2
chelseah02 <chelseah2020@gmail.com>
2023-11-06 21:34:25 -0600

    Created react app with home

---
+++ streamstash/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div class="topnav">
+      <a class="active" href="#home">Home</a>
+      <a href="#news">Add Movie</a>
+      <a href="#contact">My Movies</a>
+      <a href="#about">About StreamStash</a>
+    </div>
+      <h2> StreamStash </h2>
+      <h3> No movies found yet! 😞 </h3>
+    </div>
+  );
+}
+
+export default App;
